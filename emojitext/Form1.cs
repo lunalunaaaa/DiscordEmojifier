32ecac4162edec9e63ec88a0238412ee877e0e75
Luna <72394034+lunalunaaaa@users.noreply.github.com>
2020-11-11 11:20:32 +0000

    Add files via upload

---
+++ emojitext/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace emojitext
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string complete = "";
+
+            foreach (char c in textBox1.Text.ToLower())
+            {
+                if (Char.IsLetter(c) || c == ' ')
+                {
+                    if (c == ' ')
+                    {
+                        complete += "  ";
+                    }
+                    else
+                    {
+                        complete += ":regional_indicator_" + c + ": ";
+                    }
+                }
+            }
+            textBox2.Text = complete + " .";
+        }
+    }
+}
